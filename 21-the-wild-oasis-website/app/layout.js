a0b5a17d02e767d591a6d3a6c031cbdb5c297d8b
Javenhun733r <andrij.babyuk@gmail.com>
2026-02-12 16:30:32 +0200

    added code for sections 34-36

--- 21-the-wild-oasis-website/app/layout.js
+++ 21-the-wild-oasis-website/app/layout.js
@@ -1,13 +1,14 @@
 import '@/app/_styles/globals.css';
 import { Josefin_Sans } from 'next/font/google';
 import Header from './_components/Header';
+import { ReservationProvider } from './_components/ReservationsContext';
 const josefin = Josefin_Sans({
 	subsets: ['latin'],
 	display: 'swap',
 });
 export const metadata = {
 	title: {
-		template: '%s The Wild Oasis',
+		template: '%s / The Wild Oasis',
 		default: 'Welcome / The Wild Oasis',
 	},
 	description:
@@ -21,7 +22,9 @@ export default function RootLayout({ children }) {
 			>
 				<Header />
 				<div className='flex-1 px-8  py-12 grid'>
-					<main className='max-w-7xl  mx-auto w-full'>{children}</main>
+					<main className='max-w-7xl  mx-auto w-full'>
+						<ReservationProvider>{children}</ReservationProvider>
+					</main>
 				</div>
 			</body>
 		</html>
